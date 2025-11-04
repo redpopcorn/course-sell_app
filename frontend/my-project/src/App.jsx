32df3c807451f8480e925bfffe1ba51064fb8061
ksauraj <arushgupta1905@gmail.com>
2025-11-04 23:52:54 +0530

    Initial commit

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,18 +1,18 @@
-import Home from "./components/Home.jsx";
-import login from "./components/login.jsx";
+import Home from "./components/Home";
+import Login from "./components/login";  // Changed to uppercase 'Login'
 import Signup from "./components/Signup";
 import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-  <div>
-    <Routes>
-      <Route path="/"element={<Home/>}/>
-      <Route path ="/login" element ={<Login/>}/>
-      <Route path ="/Signup" element ={<Signup/>}/>
-
-    </Routes>
-  </div>
-  );
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
