30e1a038e793ea5739a31f777203f4dff87ff7e7
ksauraj <arushgupta1905@gmail.com>
2025-11-06 15:50:50 +0530

    adi bhai  thanks for debugging

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,16 +1,17 @@
-import Home from "./components/Home";
-import Login from "./components/login";  // Changed to uppercase 'Login'
-import Signup from "./components/Signup";
-import { Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import Login from "./components/login";  // Changed to uppercase 'Login'
+// import Signup from "./components/Signup";
+// import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
-      </Routes>
+      </Routes> */}
+      <h1>fuck u</h1>
     </div>
   )
 }
