809a9749a2661312b551fca3d7f3db410b644dfa
ksauraj <arushgupta1905@gmail.com>
2025-11-15 22:11:11 +0530

    no changes just to maintain streak exams are going on

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const app = express();
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
